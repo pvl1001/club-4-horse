2f64c9b38ccb02ea44b30b78042302bcbf061d29
PavelIvanov <pavel.and.ivanov@yandex.ru>
2024-09-30 14:20:54 +0300

    add footer

--- src/widgets/Footer/ui/Footer.tsx
+++ src/widgets/Footer/ui/Footer.tsx
@@ -6,7 +6,9 @@ type Props = {}
 export function Footer({}: Props) {
     return (
         <footer className={s._}>
-            Footer
+            <div className="wrapper">
+                <p className={s.text}>Все персонажи, события и цитаты являются вымышленными и не принадлежат создателям сайта. С подробностями можно познакомиться в главе XXXIV романа Ильи Ильфа и Евгения Петрова «Двенадцать стульев».</p>
+            </div>
         </footer>
     );
 }
